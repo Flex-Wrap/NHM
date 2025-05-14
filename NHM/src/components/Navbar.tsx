47480d89a9c821527c00c42b871a1fc36b649d31
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-05-14 13:56:52 +0200

    navigation partially done

--- NHM/src/components/Navbar.tsx
+++ NHM/src/components/Navbar.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import IconDiscover from "../assets/icon-discover.svg?react";
+import IconExhibitions from "../assets/icon-exhibitions.svg?react";
+import IconMap from "../assets/icon-map.svg?react";
+import IconSchedule from "../assets/icon-events.svg?react";
 
 const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
-            <div className="navbar-item">
-                <i className="icon compass-icon"></i>
+            <NavLink to="/" className={({ isActive }) => `navbar-item ${isActive ? "active" : ""}`}>
+                <IconDiscover className="navbar-icon" />
                 <span>Discover</span>
-            </div>
-            <div className="navbar-item">
-                <i className="icon globe-icon"></i>
+            </NavLink>
+
+            <NavLink to="/exhibitions" className={({ isActive }) => `navbar-item ${isActive ? "active" : ""}`}>
+                <IconExhibitions className="navbar-icon" />
                 <span>Exhibitions</span>
-            </div>
-            <div className="navbar-item">
-                <i className="icon map-icon"></i>
+            </NavLink>
+
+            <NavLink to="/map" className={({ isActive }) => `navbar-item ${isActive ? "active" : ""}`}>
+                <IconMap className="navbar-icon" />
                 <span>Map</span>
-            </div>
-            <div className="navbar-item">
-                <i className="icon calendar-icon"></i>
+            </NavLink>
+
+            <NavLink to="/schedule" className={({ isActive }) => `navbar-item ${isActive ? "active" : ""}`}>
+                <IconSchedule className="navbar-icon" />
                 <span>Schedule</span>
-            </div>
+            </NavLink>
         </nav>
     );
 };
