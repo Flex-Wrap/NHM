508574b2425434ab79f07d05e768bd7076fb6a52
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-05-16 10:52:34 +0200

    update

--- NHM/src/pages/infoPages/TicketPrices.tsx
+++ NHM/src/pages/infoPages/TicketPrices.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export const TicketPrices: React.FC = () => {
     return (
-        <div>
+        <div className="content">
+            
             <h1>Ticket Prices</h1>
             <p>Information about ticket prices will be displayed here.</p>
         </div>
