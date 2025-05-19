9b79114b346ecf06dfa880aee4d5bb6a7f26c471
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-05-19 14:30:56 +0200

    homepage

--- NHM-website/src/components/Hero.tsx
+++ NHM-website/src/components/Hero.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React from "react";
+import heroImg from "../assets/hero1.png";
 
 const Hero: React.FC = () => {
-    return (
-        <section className="hero">
-            <div className="hero-content">
-                <h1>Welcome to NHM Website</h1>
-                <p>Your journey into natural history starts here.</p>
-            </div>
-        </section>
-    );
+  return (
+    <div className="hero">
+    <picture>
+      <img
+        className="hero__image"
+        src={heroImg}
+        alt="Hero image"
+      />
+    </picture>
+      <div className="hero__overlay"></div>
+      <div className="hero__text">
+        <h1 className="hero__title"> Expedition to <strong>Africa</strong></h1>
+        <p className="hero__description">
+          Be part of the story
+          <button className="hero__button--inline inline-button icon-link-internal"></button>
+        </p>
+        <button className="hero__button--block button button--icon--right icon-link-internal button--icon">
+          Visit Now
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
+
+
--- NHM-website/src/pages/HomePage.tsx
+++ NHM-website/src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Exhibitions from "../components/Exhibitions";
 
 const HomePage: React.FC = () => {
   return (
     <>
     <Hero />
+    <div className="page__content">
+      <Exhibitions />
+    </div>
     </>
   );
 };
